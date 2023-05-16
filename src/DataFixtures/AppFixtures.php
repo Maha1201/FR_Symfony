d8a1dd1adce5b451125315adf3f2d039cc22e8e5
Maha1201 <abdalla.m1201@gmail.com>
2023-05-16 09:31:17 +0200

    fixtures ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -27,17 +27,32 @@ class AppFixtures extends Fixture
         $manager->persist($rub4);
         $this->addReference( "Amour", $rub4);
 
-        $p1 = new Produit("fleur1", "desrpctsion", 12, "https://www.foliflora.fr/static/photos/bouquet-roses-rouges.jpg", 30);
+        $p1 = new Produit;
+        $p1->setNom("fleur1")
+            ->setDescription("Descriptoin")
+            ->setPrix(12)
+            ->setPhoto("https://www.foliflora.fr/static/photos/bouquet-roses-rouges.jpg")
+            ->setQuantiteTotale(30);
         $p1->addRubrique($this->getReference("Amour"));
         $manager->persist($p1);
 
 
-        $p2 = new Produit("fleur2", "description", 12, "https://orchisartisanfleuriste.store/wp-content/uploads/2020/05/bouquet_roses_rouges_haut_4654.jpg", 30);
+        $p2 = new Produit;
+        $p2->setNom("fleur2")
+            ->setDescription("Descriptoin")
+            ->setPrix(12)
+            ->setPhoto("https://orchisartisanfleuriste.store/wp-content/uploads/2020/05/bouquet_roses_rouges_haut_4654.jpg")
+            ->setQuantiteTotale(30);
         $p2->addRubrique($this->getReference("Deuil"));
         $manager->persist($p2);
         
 
-        $p3 = new Produit("fleur3", "description", 12, "https://cdn.shopify.com/s/files/1/0273/7415/7961/products/bouquet-de-pivoines-artificielles-rose_1200x1200.png?v=1615983423", 30);
+        $p3 = new Produit;
+        $p3->setNom("fleur3")
+            ->setDescription("Descriptoin")
+            ->setPrix(12)
+            ->setPhoto("https://cdn.shopify.com/s/files/1/0273/7415/7961/products/bouquet-de-pivoines-artificielles-rose_1200x1200.png?v=1615983423")
+            ->setQuantiteTotale(30);
         $p3->addRubrique($this->getReference("Amour"));
         $manager->persist($p3);
         
